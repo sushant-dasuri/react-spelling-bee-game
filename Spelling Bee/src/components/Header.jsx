be73ef516b404a63ad0f6cea76be039f59acb1de
sushant-dasuri <87372046+sushant-dasuri@users.noreply.github.com>
2024-09-07 21:31:53 +0530

    Adding honeycomb letters

--- Spelling Bee/src/components/Header.jsx
+++ Spelling Bee/src/components/Header.jsx
@@ -1,11 +1,10 @@
-import styles from './Header.module.css';
+export function Header({ date, editor }) {
 
-export function Header({date, editor}) {
     return (
         <header>
-            <h1 className={styles.title}>Spelling Bee</h1>
-            <p className={styles.date}>{date}</p>
-            <p className={styles.editor}>NYT game edited by {editor}</p>
+            <h1 className="title">Spelling Bee</h1>
+            <p className="date">{ date }</p>
+            <p className="editor">NYT game edited by { editor }</p>
         </header>
-    )
+    );
 }
