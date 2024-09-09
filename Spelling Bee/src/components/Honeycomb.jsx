e799e094c5a6fe8ab713ecced20883d0bbdb44f1
sushant-dasuri <87372046+sushant-dasuri@users.noreply.github.com>
2024-09-09 18:57:04 +0530

    Adding shuffle functionality

--- Spelling Bee/src/components/Honeycomb.jsx
+++ Spelling Bee/src/components/Honeycomb.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import { Letter } from "./Letter.jsx";
 
-export function Honeycomb ({centerLetter, outerLetters, validLetters}) {
-    return (
-        <article className="honeycomb">
-            <Letter letter={centerLetter} isCenter={true}></Letter>
-            <Letter letter={outerLetters[0]} isCenter={false}></Letter>
-            <Letter letter={outerLetters[1]} isCenter={false}></Letter>
-            <Letter letter={outerLetters[2]} isCenter={false}></Letter>
-            <Letter letter={outerLetters[3]} isCenter={false}></Letter>
-            <Letter letter={outerLetters[4]} isCenter={false}></Letter>
-            <Letter letter={outerLetters[5]} isCenter={false}></Letter>
-        </article>
-    )
+export function Honeycomb({ centerLetter, outerLetters, validLetters }) {
+  const [randomArray, setRandomArray] = useState([0, 1, 2, 3, 4, 5]);
+
+  const shuffle = () => {
+    setRandomArray([...randomArray].sort(() => Math.random() - 0.5));
+  };
+
+  return (
+    <>
+      <article className="honeycomb">
+        <Letter letter={centerLetter} isCenter={true}></Letter>
+        <Letter letter={outerLetters[randomArray[0]]} isCenter={false}></Letter>
+        <Letter letter={outerLetters[randomArray[1]]} isCenter={false}></Letter>
+        <Letter letter={outerLetters[randomArray[2]]} isCenter={false}></Letter>
+        <Letter letter={outerLetters[randomArray[3]]} isCenter={false}></Letter>
+        <Letter letter={outerLetters[randomArray[4]]} isCenter={false}></Letter>
+        <Letter letter={outerLetters[randomArray[5]]} isCenter={false}></Letter>
+      </article>
+      <section className="buttons">
+        <button className="button" onClick={shuffle}>
+          Shuffle
+        </button>
+      </section>
+    </>
+  );
 }
